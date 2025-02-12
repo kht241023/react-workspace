3a00e595f2fbd5a7c2335a0b14490fd71e842c63
Kyungnan Kim <kht241022@gmail.com>
2025-02-12 10:59:56 +0900

    Router proxy

--- src/index.js
+++ src/index.js
@@ -2,24 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PizzaList from "./BackendAPI/PizzaList";
+import RootPath from "./RootPath";
+import {BrowserRouter} from "react-router-dom";
 //import reportWebVitals from './reportWebVitals';
-import ChapPropsDefault from "./Chapter/ChapProps/ChapPropsDefault";
-import PracticePropsDefault from "./practice/PracticePropsDefault";
-import ChapPropsArray from "./Chapter/ChapProps/ChapPropsArray";
-import PracticePropsArray from "./practice/PracticePropsArray";
-import ChapUseEffect from "./Chapter/ChapUseEffect";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/* return 내에 사용할 수 있는 주석
+        {/* BrowserRouter 로
+        이동할 경로 를 설정했다면
+        index.js 최상단에 BrowserRouter 로
 
+        모든 javascript, javascriptXML 파일을 감싸줘야함
+
+        BrowserRouter 와 Routes 은 모든 파일에서  최초 1회만 작성
+        */}
+
+        <RootPath />
+        {/* return 내에 사용할 수 있는 주석
         App.js 는 필수로 사용해야하는 자바스크립트 파일이 아님
         만일 App.js 가 아니라 다른 파일을 사용하길 원한다면
         파일명.js 를 import 해서 사용할 수 있음
-        */}
-        <App/>
-        {/*
         <ChapUseState/>
         <PracticeUseState />
         <ChapProps />
@@ -30,9 +35,8 @@ root.render(
          <PracticePropsDefault />
          <ChapPropsArray />
         <PracticePropsArray />
-        */}
         <ChapUseEffect />
-
+        */}
 
 
     </React.StrictMode>
---
+++ src/practice/PracticeUseEffectAxios.jsx
@@ -0,0 +1,81 @@
+/*
+package.json 에서
+    dependencies" 내부에
+        "axios" 가 작성되어있다면
+            더이상 npm i axios 를 하지 않아도 됨
+            설치가 되어있기 때문에 my-app 폴더 내에서
+            언제든지 axios 를 사용할 수 있음
+*/
+
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const APIDog = () => {
+    const [dog, setDog] = useState("");
+
+    // axios 형식으로
+    // https://api.thedogapi.com/v1/images/search
+    // 최초 1회 강아지사진 랜덤 출력 후
+    // 강아지 사진 새로고침 버튼을 클릭했을 때
+    // 새로운 강아지 사진이 보일 수 있도록 코드 설정
+
+
+    const dogBtn = () => {
+            axios
+                .get("https://api.thedogapi.com/v1/images/search")
+                .then(
+                    (res) => {
+                        setDog(res.data[0].url);
+                    }
+                )
+    }
+
+
+    useEffect(() => {
+        dogBtn();
+    }, []);
+    return(
+        <>
+            <h1>랜덤 강아지 사진</h1>
+            {dog ? <img src={dog} /> : <p>로딩중</p>}
+            <button onClick={dogBtn}>강아지 사진 새로고침</button>
+        </>
+    )
+
+}
+
+
+const APIRandomJoke = () => {
+    const [setup, setSetup] = useState("");
+    const [delivery, setDelivery] = useState("");
+    useEffect(() => {
+
+        //  jokeapi = []로 이루어진 배열 형태가아님!
+        // category , setup , delivery
+        axios
+            .get("https://v2.jokeapi.dev/joke/Any")
+            .then(
+                (res) => {
+                    setSetup(res.data.setup)
+                }
+            )
+    }, []);
+    return(
+        <>
+        <h1>랜덤으로 농담 설정하기</h1>
+            {setup ? <p>{setup}</p> : <p>로딩중</p>}
+            {delivery ? <p>delivery</p> : <p>로딩중</p>}
+        </>
+    )
+}
+
+const PracticeUseEffectAxios = () => {
+    return(
+        <>
+            {/*<APIDog />*/}
+            <APIRandomJoke />
+        </>
+    )
+}
+
+export  default  PracticeUseEffectAxios;
