e2defa439f710d393e6061c4429bd67f499f6911
Kyungnan Kim <kht241022@gmail.com>
2025-02-12 12:32:24 +0900

    axios get을 이용하여 GeneralStore api 연결 설정

--- src/BackendAPI/PizzaDetail.jsx
+++ src/BackendAPI/PizzaDetail.jsx
@@ -1,11 +1,40 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useState} from "react";
 
 
 const PizzaDetail = () =>{
-    
+    const {id} = useParams(); //URL 에서 ID 값 가져오기
+    // ajax 에서 URLSearchParams 와 같은 역할
+    const [pizza, setPizza] = useState(null);
+
+    // 현재 위치를 기반으로 하여 앞으가기 뒤로가기 와 같은 설정
+    /*
+    hook
+
+    useHistory() 명칭으로 동작 -> useNavigate() 명칭으로 도입
+
+    useNavigate() React Router 에서 제공하는 페이지 이동 함수
+
+    navigate(-1) : 뒤로 가기
+    navigate(1) : 앞으로 가기
+    navigate("/") : 홈으로 이동
+    navigate("경로설정") : Link 대신 위치 이동
+    -> window.location.href 동일한 기능
+    */
+    const navigate = useNavigate();
+
+    const backBtn = () => {
+        navigate(-1);
+    }
+    // Springboot GeneralStore 에서 @RequestMapping("/api") @GetMapping("/pizzas/{id}") 참조해서
+    // axios get 을 이용해서 상세정보 데이터 호출
     return(
         <>
             <h1>피자 상세보기</h1>
-        
+            <p>가격 : pizza.price 원</p>
+            <p>피자 설명 상세보기 : description 피자 정보 가져올 수 있도록 설정</p>
+
+            <button onClick={ backBtn }>뒤로가기</button>
         </>
     )
 }
--- src/RootPath.jsx
+++ src/RootPath.jsx
@@ -2,16 +2,20 @@
 // 경로를 설정해주는 js 파일은 function 을 많이 사용
 
 //const RootPath = () => {
+
+// Switch -> Route 명칭 변환 도입
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import PizzaList from "./BackendAPI/PizzaList";
 import PizzaDetail from "./BackendAPI/PizzaDetail";
-
+// React 에서 작성하는 경로는 view Controller 와 관계 없음
+// springboot resource 밑에 작성한 css / js / html 파일은 더이상 의미가 없음
+// 위 역할을 react 에서 모두 할 것이기 때문
 function RootPath() {
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<PizzaList />} />
-                <Route path="/pizzas/detail:id" element={<PizzaDetail />} />
+                <Route path="/pizzas/detail/:id" element={<PizzaDetail />} />
             </Routes>
         </Router>
     )
