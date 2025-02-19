02070d95f627254ed40c47a0feebd599ecd67582
Kyungnan Kim <kht241022@gmail.com>
2025-02-19 10:48:52 +0900

    Company Component, Page 설정

---
+++ user-role/src/pages/AdminComponent.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const AdminComponent = () => {
+    return (
+        <div style={{ backgroundColor: "#ffcccc", padding: "20px", borderRadius: "5px" }}>
+            <h2>📢 관리자 전용 대시보드</h2>
+            <p>이곳에서 모든 관리 기능을 사용할 수 있습니다.</p>
+        </div>
+    );
+};
+
+export default AdminComponent;
+// AdminComponent
+
+
+//
